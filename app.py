a2cac602e38400ccb9ebe183b98a6b2ff3bfc0da
Benjamin AZOULAY <25954316+benjyazoulay@users.noreply.github.com>
2024-10-26 13:21:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from streamlit_folium import st_folium
 import plotly.graph_objects as go
 
 # Configuration de la page Streamlit pour utiliser toute la largeur
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",page_title="Mating App")
 
 # Suppression des marges par défaut de Streamlit
 st.markdown("""
